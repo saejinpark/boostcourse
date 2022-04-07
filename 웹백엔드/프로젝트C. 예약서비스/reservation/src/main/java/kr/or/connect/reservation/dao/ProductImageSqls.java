02f56e1effb63d64119e95c28685e7266bc1b213
saejinpark951210 <saejinpark951210@gmail.com>
2022-04-07 17:30:48 +0900

    파일 변경

--- 웹백엔드/프로젝트C. 예약서비스/reservation/src/main/java/kr/or/connect/reservation/dao/ProductImageSqls.java
+++
@@ -1,5 +0,0 @@
-package kr.or.connect.reservation.dao;
-
-public class ProductImageSqls {
-	public static final String PRODUCT_IMAGE__SELECT_BY__PRODUCT_ID = "SELECT * FROM product_image WHERE product_id = :productId";
-}
