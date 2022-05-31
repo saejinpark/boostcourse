8246b43118aabb7a1b562639c7c7f8f52c0064c3
saejinpark951210 <saejinpark951210@gmail.com>
2022-05-31 19:45:44 +0900

    웹 백엔드 프로젝트 진행중

---
+++ 웹백엔드/프로젝트D. 예약-로그인/securityReservation/src/main/java/org/edwith/webbe/securityReservation/config/ApplicationConfig.java
@@ -0,0 +1,59 @@
+package org.edwith.webbe.securityReservation.config;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+// 레이어드 아키텍처에서 Controller가 사용하는 Bea들에 대해 설정을 한다.
+// dao, service를 컴포넌트 스캔하여 찾도록 한다.
+// 어노테이션으로 트랜잭션을 관리하기 위해 @EnableTransactionManagement를 설정하였다.
+@Configuration
+@ComponentScan(basePackages = {"org.edwith.webbe.securityReservation.dao", "org.edwith.webbe.securityReservation.service"})
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
+public class ApplicationConfig implements TransactionManagementConfigurer {
+
+	@Value("${spring.datasource.driver-class-name}")
+	private String driverClassName;
+
+	@Value("${spring.datasource.url}")
+	private String url;
+
+	@Value("${spring.datasource.username}")
+	private String username;
+
+	@Value("${spring.datasource.password}")
+	private String password;
+    
+    @Bean
+    public DataSource dataSource() {
+    	BasicDataSource dataSource = new BasicDataSource();
+    	dataSource.setDriverClassName(driverClassName);
+    	dataSource.setUrl(url);
+    	dataSource.setUsername(username);
+    	dataSource.setPassword(password);
+    	
+    	return dataSource;
+    }
+    
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+    	return new DataSourceTransactionManager(dataSource());
+    }
+    
+	@Override
+	public TransactionManager annotationDrivenTransactionManager() {
+		return transactionManager();
+	}
+
+}
