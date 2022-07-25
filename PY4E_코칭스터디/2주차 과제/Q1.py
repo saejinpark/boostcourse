cffb79f0423a288d6474e8eecfbf264cdb7c7bbe
saejinpark951210 <saejinpark951210@gmail.com>
2022-07-25 11:55:12 +0900

    미션 수정

--- PY4E_코칭스터디/2주차 과제/Q1.py
+++ PY4E_코칭스터디/2주차 과제/Q1.py
@@ -1,43 +1,44 @@
 import random
 
 ROCK_SCISSORS_PARER = ["바위", "가위", "보"]
-NUMS = [0, 1, 2]
+ROCK_SCISSORS_PARER_INDEX = [0, 1, 2]
+RESULT = ["비겼습니다!", "나 승리!", "컴퓨터 승리!"]
+ROCK_SCISSORS_PARER_DICT = {
+    "바위": {
+        "바위": 0,
+        "가위": 1,
+        "보": 2
+    },
+    "가위": {
+        "바위": 2,
+        "가위": 0,
+        "보": 1,
+    },
+    "보": {
+        "바위": 1,
+        "가위": 2,
+        "보": 0,
+    }
+}
+
+def translate_rock_scissors_paper(my):
+    if my.isdigit():
+        my = int(my)
+        if my in ROCK_SCISSORS_PARER_INDEX:
+            my = ROCK_SCISSORS_PARER[my]
+    while my not in ROCK_SCISSORS_PARER and my not in ROCK_SCISSORS_PARER_INDEX:
+        my = translate_rock_scissors_paper(input("다시 입력하세요 : "))
+    return my
+
 
 def rcp(my):
     computer = ROCK_SCISSORS_PARER[random.randint(0, 2)]
     print(f"나: {my}")
     print(f"컴퓨터: {computer}")
-    if my == "바위":
-        if computer == "바위":
-            print("비겼습니다!")
-        elif computer == "가위":
-            print("나 승리!")
-        elif computer == "보":
-            print("컴퓨터 승리!")
-    elif my == "가위":
-        if computer == "바위":
-            print("컴퓨터 승리!")
-        elif computer == "가위":
-            print("비겼습니다!")
-        elif computer == "보":
-            print("나 승리!")
-    elif my == "보":
-        if computer == "바위":
-            print("나 승리!")
-        elif computer == "가위":
-            print("컴퓨터 승리!")
-        elif computer == "보":
-            print("비겼습니다!")
+    print(RESULT[ROCK_SCISSORS_PARER_DICT[my][computer]])
 
-my = input("가위 바위 보 : ")
 
-if my.isdigit():
-    my = int(my)
+my = translate_rock_scissors_paper(input("가위 바위 보 : "))
 
-while my not in ROCK_SCISSORS_PARER and my not in NUMS:
-    my = input("다시 입력하세요 : ")
-
-if my in NUMS:
-    my = ROCK_SCISSORS_PARER[my]
 
 rcp(my)
